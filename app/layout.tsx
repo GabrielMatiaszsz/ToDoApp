c148ac18347584b29a8699c196555bbb528bb17a
Gabriel Matias <gabrielmatias@MacBook-Pro-de-Gabriel.local>
2024-03-10 22:25:37 -0300

    Comit inicial

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/providers/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ToDoList",
+  description: "App criado para aprendizado e testes",
+icons:{
+  icon:[
+    {
+      media:"(prefers-color-scheme: ligth)",
+      url: "./logo.svg",
+      href: "./logo.svg",
+    },
+    {
+      media:"(prefers-color-scheme: dark)",
+      url: "/logo-dark.svg",
+      href: "/logo-dark.svg",
+    }
+  ]
+}
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+ attribute="class"
+ defaultTheme="system"
+ enableSystem
+ disableTransitionOnChange
+ storageKey="jotion-theme-2"
+>
+{children}
+</ThemeProvider>
+      </body>
+    </html>
+  );
+}
