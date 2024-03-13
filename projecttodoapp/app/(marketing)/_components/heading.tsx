d81485e75659832d4ac9eeba6e34de880488d083
gabriel matias <gmsilva1@uniara.edu.br>
2024-03-13 20:03:33 -0300

    esqueleto da pagina de marketing pronto

--- projecttodoapp/app/(marketing)/_components/heading.tsx
+++ projecttodoapp/app/(marketing)/_components/heading.tsx
@@ -6,18 +6,18 @@ import { ArrowRight } from "lucide-react";
 export const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
-            <h1 className="text-3xl sm:text-6xl font-bold"> 
-                Suas ideias, pensamentos e anotaçoes em um unico lugar, conheça o <span className="underline">YouList! </span>
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+                Suas ideias & planos organizados da melhor maneira. Bem vindo ao{" "}
+                <span className="underline">Todoapp!</span>
             </h1>
-            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                Youlist é um app para você anotar suas ideias<br/>de forma organizada e rapida!
+            <h3 className="text-base sm:text-xl md:text-2-xl font-medium">
+                Todoapp é um ambiente conectado de para você trabalhar
+                <br /> melhor, mais rapido e feliz!
             </h3>
             <Button>
-                Acesse aqui
-                <ArrowRight className= "h-4 w-4 ml-2"/>
+               Clique aqui
+                <ArrowRight className="h-4 w-4 ml-2" />
             </Button>
-           
-
         </div>
-    )
+    );
 }
--- projecttodoapp/app/layout.tsx
+++ projecttodoapp/app/layout.tsx
@@ -1,27 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ToDoList App",
-  description: "Um lugar para voce compartilhar suas ideias",
-  icons:{
-    icon:[
-      {
-        media:"prefers-color-scheme: light",
-        url: "/logo.svg",
-        href:"/logo.svg"
-      },
-      {
-        media:"prefers-color-scheme: dark",
-        url: "/logo-dark.svg",
-        href:"/logo-dark.svg"
-      }
-    ]
-  }
-};
+  title: "ToDoList",
+  description: "App criado para aprendizado e testes",
+icons:{
+  icon:[
+    {
+      media:"(prefers-color-scheme: ligth)",
+      url: "./logo.svg",
+      href: "./logo.svg",
+    },
+    {
+      media:"(prefers-color-scheme: dark)",
+      url: "/logo-dark.svg",
+      href: "/logo-dark.svg",
+    }
+  ]
+}
+}
 
 export default function RootLayout({
   children,
@@ -30,7 +31,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+ attribute="class"
+ defaultTheme="system"
+ enableSystem
+ disableTransitionOnChange
+ storageKey="jotion-theme-2"
+>
+{children}
+</ThemeProvider>
+      </body>
     </html>
   );
 }
